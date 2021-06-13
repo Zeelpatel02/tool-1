018abe04d08f5007f59bfc4d2c5a98018de18b65
jaykakadiya18 <jaykakadiya2014@gmail.com>
2021-06-13 14:41:49 +0530

    V2

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('',views.index, name="Home")
-]
+    path('',views.index, name="Home"),
+    path('tool',views.tool, name="Tool"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,27 @@
+from django.shortcuts import render, HttpResponse
+from .models import FilesUpload
+
+#add manualy
+import os
+from django.conf import settings
 from django.shortcuts import render
+from django.templatetags.static import static
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method =="POST":
+        file = request.FILES["file"]
+        document = FilesUpload.objects.create(file=file)
+        document.save()
+        # message = "Your File is Succesfully uploaded..."
+        # return render(request,"index.html",{"messg":message})
+        return render(request, 'index.html')
+    path = settings.MEDIA_ROOT
+    pdf_list = os.listdir(path + '/')
+    context = {'pdfs' : pdf_list}
+    return render(request, "index.html", context)
+def tool(request):
+    path = settings.MEDIA_ROOT
+    pdf_list = os.listdir(path + '/')
+    context = {'pdfs' : pdf_list[::-1]}
+    return render(request, 'tool.html', context)
