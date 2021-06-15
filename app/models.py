7dfe7515446f4240a6a469cf4ed19114ea3c667c
Jay Kakadiya <58882863+jaykakadiya18@users.noreply.github.com>
2021-06-15 23:44:33 +0530

    Update models.py
    File name add in databse

--- app/models.py
+++ app/models.py
@@ -3,4 +3,13 @@ from django.db import models
 # Create your models here.
 class FilesUpload(models.Model):
     file = models.FileField()
+    def __str__(self):
+        return self.file
 
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+
+@receiver(pre_delete, sender=FilesUpload)
+def mymodel_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.file.delete(False)
