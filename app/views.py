cbc1a79661ee85aebb57dedb9263457bd47e02dc
Jay Kakadiya <58882863+jaykakadiya18@users.noreply.github.com>
2021-06-21 19:03:23 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import FilesUpload
 
+
 #add manualy
 import os
 from django.conf import settings
@@ -24,11 +25,13 @@ def index(request):
     context = {'pdfs' : pdf_list}
     return render(request, "index.html", context)
 def tool(request, num):
+
     path = settings.MEDIA_ROOT
     pdf_list = os.listdir(path + '/')
     num = num
     zippedlist = zip(pdf_list, range(1,len(pdf_list)+1))
-    context = {'pdfs' : pdf_list[::-1], "var" : pdf_list[int(num-1)] , "lpdf" : zippedlist, "next" : num+1, "back" : num-1 ,"lastnum" : len(pdf_list)+1}
+    pages = list(range(1,21)) #add this line
+    context = {"page":pages,"num" : num , 'pdfs' : pdf_list[::-1], "var" : pdf_list[int(num-1)] , "lpdf" : zippedlist, "next" : num+1, "back" : num-1 ,"lastnum" : len(pdf_list)+1}
     return render(request, 'tool.html', context)
 
 
